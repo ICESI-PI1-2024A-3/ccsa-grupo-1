0eed6bf3722894e2c5ca0759c0b6d1d6ca11a527
EstebanGZam <estebangaviria82@gmail.com>
2024-03-09 22:43:40 -0500

    feat: functional login form by `post` method

--- AcademicProgrammingApplication/forms.py
+++ AcademicProgrammingApplication/forms.py
@@ -2,12 +2,12 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    name = forms.CharField(label="Nombre de usuario",
-                           max_length=200,
+    name = forms.CharField(max_length=200,
                            widget=forms.TextInput(
-                               attrs={'class': 'username', 'placeholder': 'Ingrese su nombre de usuario'})
+                               attrs={'id': 'username-input', 'placeholder': 'Ingrese su nombre de usuario'})
                            )
-    password = forms.CharField(label="Contraseña",
+    password = forms.CharField(label='Contraseña',
                                max_length=200,
-                               widget=forms.TextInput(attrs={'class': '', 'placeholder': 'Ingrese su contraseña'})
+                               widget=forms.PasswordInput(
+                                   attrs={'placeholder': 'Ingrese su contraseña'})
                                )
--- AcademicProgrammingApplication/views.py
+++ AcademicProgrammingApplication/views.py
@@ -4,6 +4,7 @@ from .forms import UserForm
 
 # Create your views here.
 def login(request):
+    # print(request.POST)
     return render(request, 'login.html', {
         'form': UserForm()
     })
