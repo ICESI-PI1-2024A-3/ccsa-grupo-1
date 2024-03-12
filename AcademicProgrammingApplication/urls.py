d00aa6dc562f5df6bc44d1271c916e2996a3aa71
Juanma33 <mipus2005@gmail.com>
2024-03-12 10:34:10 -0500

    style: previous design of the style to visualize information about a subject

--- AcademicProgrammingApplication/urls.py
+++ AcademicProgrammingApplication/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
+    path('subject/<int:subject_id>/', views.subject_detail, name='subject_detail'),
     # Lines used to see how the base HTML looks like
-    # path('base', views.base_screen, name='base'),
+    path('base', views.base_screen, name='base'),
 ]
--- AcademicProgrammingApplication/views.py
+++ AcademicProgrammingApplication/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import UserForm
-
+from .models import Subject
 
 # Create your views here.
 def login(request):
@@ -9,10 +9,13 @@ def login(request):
         'form': UserForm()
     })
 
+def subject_detail(request, subject_id):
+    subject = Subject.objects.get(code=subject_id)
+    return render(request, 'subject_detail.html', {'subject': subject,                                         'title': 'Gestión de MATERIA'})
 
 # The next lines are only used to see how the base HTML looks like
-# def base_screen(request):
-#     return render(request, 'layouts/base-app-pages.html', {
-#         'user_name': "Carlos",
-#         'title': 'Main page',
-#     })
+def base_screen(request):
+    return render(request, 'layouts/base-app-pages.html', {
+        'user_name': "Carlos",
+        'title': 'Main page',
+    })
