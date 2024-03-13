be060478614d30c81545fe3259e9b1253534df03
Dantesiio <DavidDo29@hotmail.com>
2024-03-12 21:37:53 -0500

    style: preview version of the edit class session function

--- AcademicProgrammingApplication/urls.py
+++ AcademicProgrammingApplication/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.login, name='login'),
     # Lines used to see how the base HTML looks like
-    # path('base', views.base_screen, name='base'),
+    path('edit', views.edit_screen, name='edit'),
+
+
+#This will change     
+#   path('editSubject/<int:nrc>',SubjectFormView.edit, name='editSubject'),
 ]
--- AcademicProgrammingApplication/views.py
+++ AcademicProgrammingApplication/views.py
@@ -11,8 +11,20 @@ def login(request):
 
 
 # The next lines are only used to see how the base HTML looks like
-# def base_screen(request):
-#     return render(request, 'layouts/base-app-pages.html', {
+def base_screen(request):
+     return render(request, 'layouts/base-app-pages.html', {
 #         'user_name': "Carlos",
-#         'title': 'Main page',
-#     })
+         'title': 'Main page',
+     })
+
+def edit_screen(request):
+     return render(request, 'layouts/base-app-pages.html', {
+         'user_name': "David",
+         'title': 'Editor de materia',
+     })
+#prototype
+def edit(request, nrc):
+     subject = subject.objects.filter(id=nrc).first()
+     form = SubjectForm(instance=subject)
+     return render(request, "Edit-info-class.html", {"form":form, "subject":subject})
+
