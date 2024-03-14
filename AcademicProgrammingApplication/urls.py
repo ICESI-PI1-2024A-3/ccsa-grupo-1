c08d7bdadae1258e935095430f708924771142a2
EstebanGZam <estebangaviria82@gmail.com>
2024-03-14 08:36:57 -0500

    feat: implementation of the sign-up

--- AcademicProgrammingApplication/urls.py
+++ AcademicProgrammingApplication/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
+    path('sing-up', views.sing_up, name='sing-up'),
     # Lines used to see how the base HTML looks like
     path('base', views.base_screen, name='base'),
-    path('management', views.academic_management, name='academic-management')
+    path('home', views.academic_management, name='academic-management')
 ]
--- AcademicProgrammingApplication/views.py
+++ AcademicProgrammingApplication/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login as auth_login
+from django.db import IntegrityError
 
 
 # Create your views here.
@@ -23,3 +27,27 @@ def academic_management(request):
         'user_name': 'Esteban',
         'title': 'Academic Management',
     })
+
+
+def sing_up(request):
+    if request.method == 'GET':
+        return render(request, 'sing-up.html', {
+            'form': UserCreationForm,
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'],
+                                                password=request.POST['password1'])
+                user.save()
+                auth_login(request, user)
+                return redirect('academic-management')
+            except IntegrityError:
+                return render(request, 'sing-up.html', {
+                    'form': UserCreationForm,
+                    'error': 'Username already exists!',
+                })
+        return render(request, 'sing-up.html', {
+            'form': UserCreationForm,
+            'error': 'Password does not match!',
+        })
