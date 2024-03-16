852b0d760e217993ea199e3189c9d3617c2efb59
EstebanGZam <estebangaviria82@gmail.com>
2024-03-16 18:31:29 -0500

    feat: login operation implemented

--- AcademicProgrammingApplication/views.py
+++ AcademicProgrammingApplication/views.py
@@ -4,15 +4,28 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import authenticate
 from django.db import IntegrityError
 
 
 # Create your views here.
 def login(request):
-    # print(request.POST)
-    return render(request, 'login.html', {
-        'form': UserForm()
-    })
+    if request.method == 'GET':
+        return render(request, 'login.html', {
+            'form': UserForm()
+        })
+    else:
+        # Method that helps me verify if the data obtained in the login is correct
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'login.html', {
+                'form': UserForm(),
+                'error': 'Incorrect username or password',
+            })
+        else:
+            auth_login(request, user)
+            return redirect('academic-management')
 
 
 # The next lines are only used to see how the base HTML looks like
