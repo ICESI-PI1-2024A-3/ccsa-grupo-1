fb1cbb77eea7f6d423296fe219f57e6a2d000d96
EstebanGZam <estebangaviria82@gmail.com>
2024-03-26 13:17:29 -0500

    feat: population of the database with simulated data

--- AcademicProgrammingApplication/admin.py
+++ AcademicProgrammingApplication/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Subject, Class, Teacher
 
+# Register your models here.
 admin.site.register(Subject)
 admin.site.register(Class)
 admin.site.register(Teacher)
-# Register your models here.
