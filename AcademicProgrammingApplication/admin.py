ba837f3bd27fef33c1fe70ad9c0b30cb4d561c4b
EstebanGZam <estebangaviria82@gmail.com>
2024-03-26 23:11:18 -0500

    feat: graduate program search engine
    implementation of the graduate program search engine and the list of program subjects with links to their corresponding detail page

--- AcademicProgrammingApplication/admin.py
+++ AcademicProgrammingApplication/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Subject, Class, Teacher
+from .models import Subject, Class, Teacher, Program
 
 # Register your models here.
 admin.site.register(Subject)
 admin.site.register(Class)
 admin.site.register(Teacher)
+admin.site.register(Program)
--- AcademicProgrammingApplication/views/academic_management.py
+++ AcademicProgrammingApplication/views/academic_management.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from django.db.models import Q
+from AcademicProgrammingApplication.models import Semester, Program
 
 
 def academic_management(request):
-    program = request.GET.get('program')
-    academic_period = request.GET.get('academic-period')
-    print(program, academic_period)
     user = request.user
+    queryset = request.GET
+    program = queryset.get('program')
+    semester = Semester.objects.get(period=queryset.get('semester'))
+    program_information = None
+    if queryset:
+        program_information = Program.objects.filter(Q(name__icontains=program) & Q(semesters__in=[semester])).first()
+
     return render(request, 'academic-management.html', {
         'title': 'Programación académica',
         'user_name': user.username,
+        'program_information': program_information,
+        'semester': semester,
     })
