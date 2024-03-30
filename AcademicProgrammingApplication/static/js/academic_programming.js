87371444ac56d314bed381993ecd5e9f184e9316
EstebanGZam <estebangaviria82@gmail.com>
2024-03-30 00:10:00 -0500

    docs: comments added to code of the academic programming view

--- AcademicProgrammingApplication/static/js/academic_programming.js
+++ AcademicProgrammingApplication/static/js/academic_programming.js
@@ -1,9 +1,14 @@
 (function () {
+    // Select all elements with the class "removal-btn" and store them in a constant
     const removal_btn = document.querySelectorAll(".removal-btn");
 
+    // Iterate over each element with the class "removal-btn"
     removal_btn.forEach(btn => {
+        // Prevent the default behavior of the click event
         btn.addEventListener("click", function (e) {
+            // Prevent the default behavior of the click event
             e.preventDefault();
+            // Display a SweetAlert modal with a confirmation message
             Swal.fire({
                 title: "¿Está seguro de que desea eliminar el programa de posgrado?",
                 text: "Esta acción no se puede revertir.",
@@ -13,9 +18,11 @@
                 confirmButtonColor: "#d33",
                 backdrop: true,
                 showLoaderOnConfirm: true,
+                // When the user confirms, redirect to the URL specified in the button's href attribute
                 preConfirm: () => {
                     location.href = e.target.href;
                 },
+                // Prevent the modal from being closed by clicking outside of it or pressing the escape key
                 allowOutsideClick: () => false,
                 allowEscapeKey: () => false,
             });
--- AcademicProgrammingApplication/views/academic_management.py
+++ AcademicProgrammingApplication/views/academic_management.py
@@ -1,25 +1,41 @@
+# Import necessary modules
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from AcademicProgrammingApplication.models import Semester, Program
 
 
+# Define a view function for managing academic programming
 def academic_management(request):
+    # Retrieve the authenticated user
     user = request.user
+
+    # Get query parameters from the request
     queryset = request.GET
+
+    # Initialize variables
     program_information = None
     semester = None
     subjects = request.GET.get('subject_search')
+
+    # Check if query parameters exist
     if queryset:
         try:
+            # Extract program and semester from query parameters
             program = queryset.get('program')
             semester = Semester.objects.get(period=queryset.get('semester'))
+
+            # Query the database for program information based on program name and semester
             program_information = Program.objects.filter(
                 Q(name__icontains=program) & Q(semesters__in=[semester])).first()
+
+            # Filter subjects based on search query or retrieve all subjects for the program
             if subjects:
                 subjects = program_information.subjects.filter(name__icontains=subjects).distinct()
             else:
                 subjects = program_information.subjects.all()
+
         except AttributeError:
+            # Render error message if program or semester is not found
             return render(request, 'academic-management.html', {
                 'title': 'Programación académica',
                 'user_name': user.username,
@@ -27,6 +43,7 @@ def academic_management(request):
                          'verifica que la información ingresada sea correcta e intentalo nuevamente.',
             })
 
+    # Render the academic management page with necessary context data
     return render(request, 'academic-management.html', {
         'title': 'Programación académica',
         'user_name': user.username,
@@ -36,7 +53,13 @@ def academic_management(request):
     })
 
 
+# Define a view function for deleting an academic program
 def delete_academic_program(request, program_id):
+    # Retrieve the program instance to be deleted
     program = Program.objects.get(id=program_id)
+
+    # Delete the program
     program.delete()
+
+    # Redirect the user to the home page
     return redirect('home')
