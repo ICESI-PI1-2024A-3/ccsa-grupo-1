785349aeabcef43488aa4ae7906a95db373d16d9
EstebanGZam <estebangaviria82@gmail.com>
2024-03-30 23:08:24 -0500

    styles: improved space request view styles

--- AcademicProgrammingApplication/static/js/request_classroom.js
+++ AcademicProgrammingApplication/static/js/request_classroom.js
@@ -1,62 +1,62 @@
 document.getElementById('miBoton').addEventListener('click', function () {
 
     Swal.fire({
-        title: 'Selecciona una fecha y hora',
         html: `
-      <p><input id="datetime1" type="datetime-local" class="swal-input"></p>
-      <p><input id="datetime2" type="datetime-local" class="swal-input"></p>
-    `,
+            <!-- Bootstrap style sheet for CSS-->
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+            <div class="input-group mb-4 mt-4">
+                <span class="input-group-text">Tipo de espacio</span>
+                <select class="form-select" name="classroom_type" id="select">
+                    <option value="Aula" selected>Aula</option>
+                    <option value="Laboratorio">Laboratorio</option>
+                    <option value="Auditorio">Auditorio</option>
+                </select>
+            </div>
+            <div class="input-group mb-4">
+                <span class="input-group-text">Fecha de inicio</span>
+                <input id="datetime1" type="datetime-local" class="form-control">
+            </div>
+            <div class="input-group">
+                <span class="input-group-text">Fecha de finalización</span>
+                <input id="datetime2" type="datetime-local" class="form-control">
+            </div>
+        `,
+        title: 'Reserva de Espacios',
         showCancelButton: true,
-        confirmButtonText: 'Aceptar',
         cancelButtonText: 'Cancelar',
-        input: 'select',
-        inputPlaceholder: 'Selecciona tipo de salón',
-        inputValue: '',
-        inputOptions: {
-            'Aula': 'Aula',
-            'Laboratorio': 'Laboratorio',
-            'Auditorio': 'Auditorio'
-        },
+        cancelButtonColor: 'rgba(108, 117, 125)',
+        confirmButtonText: 'Aceptar',
+        showConfirmButton: true,
+        confirmButtonColor: 'rgba(23, 104, 172, 0.9)',
+        reverseButtons: true,
+        allowOutsideClick: false,
         preConfirm: () => {
             const datetime1 = Swal.getPopup().querySelector('#datetime1').value;
             const datetime2 = Swal.getPopup().querySelector('#datetime2').value;
-            const salon = Swal.getPopup().querySelector('select').value;
+            const salon = Swal.getPopup().querySelector('#select').value;
             if (!datetime1 || !datetime2 || !salon) {
-                Swal.showValidationMessage('Debes seleccionar ambas fechas, horas y el tipo de salón');
+                Swal.showValidationMessage('Debes seleccionar tanto las fechas como las horas, así como también especificar el tipo de salón.');
             }
             return {datetime1: datetime1, datetime2: datetime2, salon: salon};
         }
     }).then((result) => {
         if (result.isConfirmed) {
-            const datos = {
+            const data = {
                 datetime1: result.value.datetime1,
                 datetime2: result.value.datetime2,
                 salon: result.value.salon
             };
-
-            // Envía los datos al backend de Django
-            fetch('/dataProcessor_lounge/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(datos)
-            }).then(response => {
-                if (response.ok) {
-                    // Si la solicitud fue exitosa, puedes realizar acciones adicionales aquí
-                    console.log('Datos enviados correctamente');
-                } else {
-                    console.error('Error al enviar datos al servidor');
-                }
-            }).catch(error => {
-                console.error('Error:', error);
+            Swal.fire({
+                html: `
+                <!-- Bootstrap style sheet for CSS-->
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+                <div class="p-1">Se le notificará por correo electrónico una vez la Oficina de Planeación Académica apruebe su solicitud.</div>
+                `,
+                titleText: "Solicitud Enviada Exitosamente",
+                icon: "success",
+                confirmButtonText: "Aceptar",
+                confirmButtonColor: 'rgba(23, 104, 172, 0.9)',
             });
-
         }
     });
 });
-
-// Crear un elemento <style> y agregar los estilos CSS
-const styleElement = document.createElement('style');
-styleElement.innerHTML = styles;
-document.head.appendChild(styleElement);
--- AcademicProgrammingApplication/urls.py
+++ AcademicProgrammingApplication/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
          name='academic_program_edition'),
     path('home/edit_academic_program/<int:program_id>/', views.edit_academic_program,
          name='edit_academic_program'),
-    path('request_classroom', views.request_classroom, name='spam'),
+    path('request_classroom', views.request_classroom, name='request_classroom'),
 ]
