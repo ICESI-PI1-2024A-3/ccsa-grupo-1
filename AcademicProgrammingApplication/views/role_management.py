bfe072ab0b2a4f747dcb84f80fe675f94f66dfbe
EstebanGZam <estebangaviria82@gmail.com>
2024-05-01 17:35:51 -0500

    fix: fix to involuntary user change bug

--- AcademicProgrammingApplication/views/role_management.py
+++ AcademicProgrammingApplication/views/role_management.py
@@ -23,11 +23,11 @@ def role_management(request):
     show_all = total_users == filtered_users.count()
     if request.POST:
         for i in range(0, len(filtered_users)):
-            user = filtered_users[i]
-            role = Role.objects.get(name=request.POST[f'role{i + 1}'])
-            user.role = role
-            user.save()
-    # print(user.get_all_permissions())
+            user_in_list = filtered_users[i]
+            user_role = Role.objects.get(name=request.POST[f'role{i + 1}'])
+            user_in_list.role = user_role
+            user_in_list.save()
+
     return render(request, 'role-management.html', {
         'title': 'Gestión de roles',
         'change_role_permission': user.has_perm('AcademicProgrammingApplication.change_role'),
