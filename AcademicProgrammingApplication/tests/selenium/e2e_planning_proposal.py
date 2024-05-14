6c03549f72fe5e6d4f030b78622191dc4d24549b
jdColonia <JuanDavidColoniaAldana@gmail.com>
2024-05-13 20:27:20 -0500

    test: corrections in test of planning proposal

--- AcademicProgrammingApplication/tests/selenium/e2e_planning_proposal.py
+++ AcademicProgrammingApplication/tests/selenium/e2e_planning_proposal.py
@@ -1,31 +1,33 @@
 import os
 import time
-from unittest import TestCase
-from venv import logger
-
-from django.test import override_settings
-from AcademicProgrammingApplication.models import User
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from django.contrib.auth.models import User
-from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from django.core.management import call_command
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from AcademicProgrammingApplication.models import PlanningProposal
 
-    
-class PlanningProposalE2ETest(StaticLiveServerTestCase):
+class PlanningProposalTest(StaticLiveServerTestCase):
+    databases = {'default': 'test'}
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        call_command('loaddata', 'test.json')
+        call_command('loaddata', 'permissions.json')
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
     def setUp(self):
         super().setUp()
-        self.driver = WebDriver()
-        self.driver.implicitly_wait(10)
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
 
     def tearDown(self):
         self.driver.quit()
         super().tearDown()
 
-    @override_settings(DEBUG=True)
     def test_upload_file(self):
         # Log in
         self.driver.get(self.live_server_url)
@@ -36,29 +38,91 @@ class PlanningProposalE2ETest(StaticLiveServerTestCase):
         submit_button = self.driver.find_element("id", 'access')
         submit_button.click()
 
-        # Upload a file
-
         # Navigate to the page
         self.driver.get(self.live_server_url + '/planning_proposal')
         file_path = '../ccsa-grupo-1/test_file/info_de_Banner.xlsx'
         absolute_file_path = os.path.abspath(file_path)
-        print(f'Ruta absoluta del archivo: {absolute_file_path}')
 
         file_input = self.driver.find_element("name", 'file')
         file_input.send_keys(absolute_file_path)
 
-    
-
-        # Check if the update button is displayed and enabled
-        update_button = self.driver.find_element("name", 'action')
-        self.assertTrue(update_button.is_displayed(), "El botón de actualización no está visible")
-        self.assertTrue(update_button.is_enabled(), "El botón de actualización no está habilitado")
-
         # Click the update button
+        update_button = self.driver.find_element("name", 'action')
         update_button.click()
-        time.sleep(2)
+        time.sleep(10)
 
-    
-                
+        # Verification of the contents of the file
+        teacher1_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'Javier Adolfo Aguirre Ramos')]"))
+        )
+        self.assertTrue(teacher1_element.is_displayed())
 
-                
+        subject1_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'Diseño para la innovación social')]"))
+        )
+        self.assertTrue(subject1_element.is_displayed())
+
+        date1_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//td[contains(text(), '12 de enero de 2024 a las 00:00')]"))
+        )
+        self.assertTrue(date1_element.is_displayed())
+
+        editor1_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'admin')]"))
+        )
+        self.assertTrue(editor1_element.is_displayed())
+
+        comment1_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'La clase fue movida por cuestion de que el profesor no pudo llegar')]"))
+        )
+        self.assertTrue(comment1_element.is_displayed())
+
+        teacher2_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'Ana Carolina Martinez Romero')]"))
+        )
+        self.assertTrue(teacher2_element.is_displayed())
+
+        subject2_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'Creación de empresa: de la idea a la implementación')]"))
+        )
+        self.assertTrue(subject2_element.is_displayed())
+
+        date2_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//td[contains(text(), '1 de marzo de 2024 a las 00:00')]"))
+        )
+        self.assertTrue(date2_element.is_displayed())
+
+        editor2_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'admin')]"))
+        )
+        self.assertTrue(editor2_element.is_displayed())
+
+        comment2_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'El profesor no pudo dictar la clase y se dicto el dia siguiente')]"))
+        )
+        self.assertTrue(comment2_element.is_displayed())
+
+        teacher3_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'Ana Carolina Martinez Romero')]"))
+        )
+        self.assertTrue(teacher3_element.is_displayed())
+
+        subject3_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'Creación de empresa: de la idea a la implementación')]"))
+        )
+        self.assertTrue(subject3_element.is_displayed())
+
+        date3_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//td[contains(text(), '15 de marzo de 2024 a las 00:00')]"))
+        )
+        self.assertTrue(date3_element.is_displayed())
+
+        editor3_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'David')]"))
+        )
+        self.assertTrue(editor3_element.is_displayed())
+
+        comment3_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'Cancelado por inundacion en la Universidad')]"))
+        )
+        self.assertTrue(comment3_element.is_displayed())
